644ce5b9364ede93106316dcfaa4671f32e281ed
PotroChen <1400539511@qq.com>
2022-01-22 23:54:03 +0800

    更改文件夹结构以及项目名称

--- AttributeCollection/Assets/Sample/Scripts/FloatRangeExample.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FloatRangeExample : MonoBehaviour
-{
-    [FloatRangeSlider(0f, 100f)]
-    public FloatRange value;
-}
