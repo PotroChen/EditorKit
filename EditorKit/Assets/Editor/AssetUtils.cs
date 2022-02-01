a46b222673c9c3a1aae3c454a07fbc30bbfd48e7
PotroChen <1400539511@qq.com>
2022-02-02 01:20:29 +0800

    添加右击 -》复制文件路径 和复制文件GUID功能

---
+++ EditorKit/Assets/Editor/AssetUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+
+public static class AssetUtils
+{
+    //[MenuItem("Assets/EditorKit", priority = 0)]
+    [MenuItem("Assets/EditorKit/CopyPath")]
+    static void CopyAssetPath()
+    {
+        string path = "";
+        if (Selection.assetGUIDs != null && Selection.assetGUIDs.Length == 1)
+        {
+            path = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+        }
+        GUIUtility.systemCopyBuffer = path;
+    }
+
+    [MenuItem("Assets/EditorKit/CopyGUID")]
+    static void CopyAssetGUID()
+    {
+        string guid = "";
+        if (Selection.assetGUIDs != null && Selection.assetGUIDs.Length == 1)
+        {
+            guid = Selection.assetGUIDs[0];
+        }
+        GUIUtility.systemCopyBuffer = guid;
+    }
+}
