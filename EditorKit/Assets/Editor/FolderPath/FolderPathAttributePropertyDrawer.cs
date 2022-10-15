69a1c6b5a5fa5eaf2372aa4c789e7ecaeaab19fa
PotroChen <1400539511@qq.com>
2022-10-15 17:24:32 +0800

    FolderPath

---
+++ EditorKit/Assets/Editor/FolderPath/FolderPathAttributePropertyDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomPropertyDrawer(typeof(FolderPathAttribute))]
+public class FolderPathAttributePropertyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		float browserBtnWidth = Mathf.Min(25f, position.width * 0.1f);
+		float leftWidth = position.width - browserBtnWidth;
+		Rect leftRect = position;
+		leftRect.width = leftWidth;
+
+		Rect rightWidth = position;
+		rightWidth.x += leftWidth;
+		rightWidth.width = browserBtnWidth;
+
+		EditorGUI.PropertyField(leftRect, property, label);
+		if(GUI.Button(rightWidth, "..."))
+		{
+			FolderPathAttribute folderPathAttribute = attribute as FolderPathAttribute;
+
+			string folder = string.IsNullOrEmpty(property.stringValue) ? folderPathAttribute.defaultPath : property.stringValue;
+			folder = EditorUtility.SaveFolderPanel("Select a folder", folder, "");
+			if(!string.IsNullOrEmpty(folder))
+				property.stringValue = folder;
+		}
+	}
+}
---
+++ EditorKit/Assets/Runtime/FolderPath/FolderPathAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolderPathAttribute : PropertyAttribute
+{
+	public string defaultPath = "";
+
+	public FolderPathAttribute()
+	{
+		
+	}
+
+	public FolderPathAttribute(string defaultPath)
+	{
+		this.defaultPath = defaultPath;
+	}
+}
